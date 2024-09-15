a6501730fdf5e41634ca8e260b94cf9dcfd0349c
Julia Bengert <yubengert@gmail.com>
2024-09-15 19:47:00 +0300

    add class Product and class Category

---
+++ src/category.py
@@ -0,0 +1,13 @@
+class Category:
+    name: str
+    description: str
+    products: list
+    product_count: int = 0
+    category_count: int = 0
+
+    def __init__(self, name, description, products_list):
+        self.name = name
+        self.description = description
+        self.products = products_list
+        Category.category_count += 1
+        Category.product_count += len(products_list)
